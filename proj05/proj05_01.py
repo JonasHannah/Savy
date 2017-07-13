3fa1b9c8136730fbd5d57677cee758b5663051e6
JonasHannah <jonashannah05@gmail.com>
2017-07-13 11:27:02 -0500

    Add files via upload

--- proj05/proj05_01.py
+++ proj05/proj05_01.py
@@ -5,7 +5,16 @@
 
 # Part I
 
-def divisors(num):
+def divisors(a):
+    answer = []
+    counter = 0
+    while counter < a:
+        counter = counter + 1
+        if a % counter == 0:
+            answer.append(counter)
+       # print "answer = ", answer
+        #print "counter = ", counter
+
     """
     Takes a number and returns all divisors of the number, ordered least to greatest
     :param num: int
@@ -13,9 +22,16 @@ def divisors(num):
     """
 
     # Fill in the function and change the return statment.
-    return 0
+    return answer
 
-def prime(num):
+def prime(a):
+    numbers = divisors(a)
+    if len(numbers) >2:
+        return False
+    else:
+        return True
+
+
     """
     Takes a number and returns True if the number is prime, otherwise False
     :param num: int
@@ -28,6 +44,12 @@ def prime(num):
 # Part II
 
 def intersection(lst1, lst2):
+    ans = []
+    for item in lst1:
+        if item in lst2 and item not in ans:
+            ans.append(item)
+      return ans
+
     """
     Takes two lists and returns a list of the elements in common between the lists
     :param lst1: list, any type
@@ -40,3 +62,100 @@ def intersection(lst1, lst2):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print ("Divisors Tests")
+# Test 1
+if divisors(1) == [1]:
+    print("Test 1: PASS")
+else:
+    print("Test 1: FAIL")
+
+# Test 2
+if divisors(8) == [1,2,4,8]:
+    print("Test 2: PASS")
+else:
+    print("Test 2: FAIL")
+
+# Test 3
+if divisors(9) == [1,3,9]:
+    print("Test 3: PASS\n")
+else:
+    print("Test 3: FAIL\n")
+
+print("Prime Tests")
+# Test 4
+if prime(9):
+    print("Test 4: FAIL")
+else:
+    print("Test 4: PASS")
+
+# Test 5
+if prime(7):
+    print("Test 5: PASS\n")
+else:
+    print("Test 5: FAIL\n")
+
+L1 = []
+L2 = [3, 4]
+L3 = [3, "a"]
+L4 = [5, "b", 10, 7, "a"]
+L5 = [5, 7, 11]
+
+print("Intersection Tests")
+# Test 6
+if intersection(L1, L2) == []:
+    print("Test 6: PASS")
+else:
+    print("Test 6: FAIL")
+
+# Test 7
+if intersection(L2, L3) == [3]:
+    print("Test 7: PASS")
+else:
+    print("Test 7: FAIL")
+
+# Test 8
+if intersection(L2, L4) == []:
+    print("Test 8: PASS")
+else:
+    print("Test 8: FAIL")
+
+# Test 9
+if intersection(L3, L4) == ["a"]:
+    print("Test 9: PASS")
+else:
+    print("Test 9: FAIL")
+
+# Test 10
+if intersection(L4, L5) == [5, 7]:
+    print("Test 10: PASS\n")
+else:
+    print("Test 10: FAIL\n")
